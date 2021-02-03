aa1a79e0dd0a9b6d6549b5840833c0daf5dce324
leeincheol <nuleeda1644@gmail.com>
2021-02-03 23:57:00 +0900

    20210203 23:56 commit

---
+++ best-page.js
@@ -0,0 +1,11 @@
+document.querySelector(".best-btn2").addEventListener('click', function(){
+    document.querySelector('.slide-container').style.transform='translate(-100vw)';
+});
+
+document.querySelector(".best-btn3").addEventListener('click', function(){
+    document.querySelector('.slide-container').style.transform='translate(-200vw)';
+});
+
+document.querySelector(".best-btn1").addEventListener('click', function(){
+    document.querySelector('.slide-container').style.transform='translate(0vw)';
+});
